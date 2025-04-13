27ab39892581ca713311213e9b06d0f41d01b35f
rag2621 <149160783+rag2621@users.noreply.github.com>
2025-04-13 17:48:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,10 +82,10 @@ const verifyToken = (req, res, next) => {
   };
 
 
-const sslOptions = {
+/*const sslOptions = {
     key: fs.readFileSync(path.join(__dirname, "server.key")), 
     cert: fs.readFileSync(path.join(__dirname, "server.cert"))
-};
+};*/
 
 
 const router = express.Router();
@@ -511,7 +511,7 @@ mongoose
     .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("✅ MongoDB Connected");
-        https.createServer(sslOptions, app).listen(PORT, () => {
+        app.listen(PORT, () => {
           console.log(`🚀 HTTPS Server running on port ${PORT}`);
       });
     })
